b5a83f9032b8c4ad5cb0050f0660dbfdc2bdab86
Mario pcDiana <hola@hola.com>
2019-05-21 02:35:45 -0500

    Agregue opciones de menu

--- palaIngles.c
+++ palaIngles.c
@@ -3,5 +3,11 @@ guardando y evaluando con opciones multiples*/
 
 int main(){
 
-return 0;
+  printf("Bienvenido, este programa teayudara a aprender paabras en ingles.");
+  printf("1 ingles a español");
+  printf("2 español a ingles");
+  printf("3 mixto");
+  printf("4 salir");
+
+  return 0;
 }
