5d674acda16f3c3fd23e327d200a8b34f5e41bd1
Mario pcDiana <hola@hola.com>
2019-05-21 22:22:26 -0500

    Agregue un Switch para las diferntes opciones

--- palaIngles.c
+++ palaIngles.c
@@ -1,9 +1,10 @@
 /*Este ptograma ayuda a aprender palablas en ingles,
 guardando y evaluando con opciones multiples*/
+
 #include <stdio.h>
 
 int main(){
-  char opcion='0';
+   char opcion='0';
    while(opcion!='4'){
     printf("Bienvenido, este programa te ayudara a aprender palabras en ingles.\n");
     printf("1 ingles a español\n");
@@ -12,6 +13,23 @@ int main(){
     printf("4 salir\n");
     scanf("%c",&opcion);
     while(getchar()!='\n');
+
+    switch((int)opcion-48){
+      case 1:
+        printf("SELECT 1\n");
+        break;
+      case 2:
+        printf("SELECT 2\n");
+        break;
+      case 3:
+        printf("SELECT 3\n");
+        break;
+      case 4:
+        printf("SELECT 4\n");
+        break;
+     default:
+        printf("INVALID OPTION\n");
+    }
   }
   return 0;
 }
